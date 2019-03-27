f3963a36ce373291ba153eb127120ec51b33e6ac
jsheradin <jsheradin@gmail.com>
2019-03-26 23:07:45 -0600

    fix build user

--- root/run.sh
+++ root/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bash
-USER=lineage
 cd /lineage
+export USER="lineage"
 export CCACHE_DIR=/ccache/
 export USE_CCACHE=1
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx20G"
